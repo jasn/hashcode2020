e8d3df47fb819089e152734629520641232627cf
Gianluca Mezzetti <mezzetti@uber.com>
2020-02-20 21:37:38 +0100

    Latest best

--- src/output_printer.go
+++ src/output_printer.go
@@ -1,27 +1,37 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+    "strings"
+)
 
 func toStdOut(output Output) {
-    fmt.Println(len(output.Libraries))
-    for _, l := range output.Libraries {
-        libraryHeader(l.ID, len(l.Books))
-        libraryBooks(l.Books)
-    }
+    print(toString(output))
 }
 
-func libraryBooks(books []BookID) {
+func toString(output Output) string {
+    var sb strings.Builder
+    sb.WriteString(fmt.Sprintf("%d\n", len(output.Libraries)))
+    for _, l := range output.Libraries {
+        libraryHeader(&sb, l.ID, len(l.Books))
+        libraryBooks(&sb, l.Books)
+    }
+    return sb.String()
+}
+
+func libraryBooks(sb *strings.Builder, books []BookID) {
     for inx, b := range books {
-        fmt.Print(b)
+        sb.WriteString(strconv.Itoa(int(b)))
         if inx < len(books) {
-            fmt.Print(" ")
+            sb.WriteString(" ")
         }
     }
-    fmt.Println("")
+    sb.WriteString("\n")
 }
 
-func libraryHeader(id LibraryID, books int) {
-    fmt.Print(id)
-    fmt.Print(" ")
-    fmt.Println(books)
+func libraryHeader(sb *strings.Builder, id LibraryID, books int) {
+    sb.WriteString(strconv.Itoa(int(id)))
+    sb.WriteString(" ")
+    sb.WriteString(strconv.Itoa(books))
 }
--- src/random_improver_test.go
+++ src/random_improver_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+var toImprove = []string{"b", "c", "d", "e", "f"}
 
 func Test__improvthemall(t *testing.T) {
-	ImproveAllSolutions([]string{"b", "c", "d", "e", "f"})
+	ImproveAllSolutions(toImprove)
+}
+
+func Test__convertthemall(t *testing.T) {
+	for _, name := range toImprove {
+		out := LoadBestOutput(name)
+		s := toString(*out)
+		err := ioutil.WriteFile(filepath.Join(dataFolder, name+".best.out"), []byte(s), 0775)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 }
