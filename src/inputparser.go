4f7f37efa6d9e027137c7bc7ed29d70f37075d32
Gianluca Mezzetti <mezzetti@uber.com>
2020-02-20 19:39:51 +0100

    Adds parser

--- src/inputparser.go
+++ src/inputparser.go
@@ -1,5 +1,14 @@
 package src
 
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+const dataFolder = "../data"
+
 type Input struct {
 	BooksTotal int
 	LibrariesTotal int
@@ -15,4 +24,73 @@ type Library struct {
 	DaysForSignUp int
 	BooksShippedPerDay int
 	Books map[BookID]struct{}
+}
+
+func LoadInput(name string) *Input {
+	dataFiles, err := ioutil.ReadDir(dataFolder)
+	if err != nil {
+		panic(err.Error())
+	}
+	fileName := ""
+	for _, file := range dataFiles {
+		if strings.HasPrefix(file.Name(), name+"_") && strings.HasSuffix(file.Name(), ".txt") {
+			fileName = file.Name()
+			break
+		}
+	}
+	if len(fileName) == 0 {
+		panic("wrong filename")
+	}
+	fileContent, err := ioutil.ReadFile(filepath.Join(dataFolder, fileName))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return Parse(string(fileContent))
+}
+
+
+func Parse(s string) *Input {
+	lines := strings.Split(s, "\n")
+	firstLine := strings.Split(lines[0], " ")
+
+	input := &Input{
+		BooksTotal:   parseInt(firstLine[0])  ,
+		LibrariesTotal: parseInt(firstLine[1]),
+		Days:           parseInt(firstLine[2]),
+	}
+
+	secondLine := strings.Split(lines[1], " ")
+	for _, bookScoreString := range secondLine {
+		input.BooksScore = append(input.BooksScore, parseInt(bookScoreString))
+	}
+
+	for i := 2; i+1 < len(lines); i+=2 {
+		firstLibraryLine := strings.Split(lines[i], " ")
+		secondLibraryLine := strings.Split(lines[i+1], " ")
+
+		if len(firstLibraryLine) < 3 {
+			continue
+		}
+
+		books := make(map[BookID]struct{}, parseInt(firstLibraryLine[0]))
+		for _, book := range secondLibraryLine {
+			books[BookID(parseInt(book))] = struct{}{}
+		}
+
+		input.Libraries = append(input.Libraries, Library{
+			DaysForSignUp:      parseInt(firstLibraryLine[1]),
+			BooksShippedPerDay: parseInt(firstLibraryLine[2]),
+			Books:              books,
+		})
+	}
+	return input
+}
+
+func parseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
 }
--- src/mezzetti_test.go
+++ src/mezzetti_test.go
@@ -1,9 +1,23 @@
 package src
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 )
 
-func Test_something(t *testing.T) {
-
+func Test_can_parse_all_examples(t *testing.T) {
+	for _, name := range []string{"a", "b", "c", "d", "e", "f"} {
+		i := LoadInput("a")
+		bytes, err := json.MarshalIndent(i, "", " ")
+		if err != nil {
+			t.FailNow()
+		}
+		err = ioutil.WriteFile(filepath.Join(dataFolder, name+".json"), bytes, 0775)
+		if err != nil {
+			t.FailNow()
+		}
+	}
 }
+
